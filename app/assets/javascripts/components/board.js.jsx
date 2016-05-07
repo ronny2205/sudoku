eca0ccd2b41ebc851f5bcf946688de4f5102f8f7
Ronny Almog <ronny2205@gmail.com>
2016-05-07 21:51:43 +0000

    creating board component

---
+++ app/assets/javascripts/components/board.js.jsx
@@ -0,0 +1,12 @@
+var Board = React.createClass({
+
+  render: function() {
+    return (
+    	<div className="row">
+    	  <div className="card-panel teal col s8 offset-s2">
+    		board
+    	  </div>
+    	</div>  
+    );
+  }
+});
--- app/assets/javascripts/components/game_page.js.jsx
+++ app/assets/javascripts/components/game_page.js.jsx
@@ -2,9 +2,14 @@ var GamePage = React.createClass({
 
   render: function() {
     return (
-    	<div className="container">
-    		<h3>Sweet sudoku</h3>
+      <div className="container">
+        <div className="section">
+    	  <h3 className="pink-text text-lighten-1 center">Sweet sudoku</h3>
     	</div>
+    	<div className="section">
+    	  <Board />
+    	</div>
+      </div>
 
     );
   }
