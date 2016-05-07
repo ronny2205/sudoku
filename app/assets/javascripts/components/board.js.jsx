c8cec2fa945f2dcc187d3441c07a83492700799e
Ronny Almog <ronny2205@gmail.com>
2016-05-07 22:09:18 +0000

    drawing nine squares

--- app/assets/javascripts/components/board.js.jsx
+++ app/assets/javascripts/components/board.js.jsx
@@ -3,8 +3,55 @@ var Board = React.createClass({
   render: function() {
     return (
     	<div className="row">
-    	  <div className="card-panel teal col s8 offset-s2">
-    		board
+    	  <div className="card-panel teal col s6 offset-s3">
+    		<div className="square">
+			    <div className="content">
+			        square
+			    </div>
+			</div>
+			<div className="square">
+			    <div className="content">
+			        square
+			    </div>
+			</div>
+			<div className="square">
+			   <div className="content">
+			       square
+			    </div>
+			</div>
+
+			<div className="square">
+			    <div className="content">
+			        square
+			    </div>
+			</div>
+			<div className="square">
+			    <div className="content">
+			        square
+			    </div>
+			</div>
+			<div className="square">
+			   <div className="content">
+			       square
+			    </div>
+			</div>
+
+			<div className="square">
+			    <div className="content">
+			        square
+			    </div>
+			</div>
+			<div className="square">
+			    <div className="content">
+			        square
+			    </div>
+			</div>
+			<div className="square">
+			   <div className="content">
+			       square
+			    </div>
+			</div>
+
     	  </div>
     	</div>  
     );
