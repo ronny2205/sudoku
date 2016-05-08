2e83c4bb44f4240e2d8308aa71af3c1f7fe4f548
Ronny Almog <ronny2205@gmail.com>
2016-05-08 00:45:21 +0000

    creating nine_number_box_component

--- app/assets/javascripts/components/board.js.jsx
+++ app/assets/javascripts/components/board.js.jsx
@@ -5,63 +5,18 @@ var Board = React.createClass({
     	<div className="row">
     	  <div className="board z-depth-3 col s6 offset-s3">
     	    <div className='square-grid'>
-			  <div className='square-grid__cell'>
-			    <div className='square-grid__content'>
-			      Some content
-			    </div>
-			  </div>
+			  <NineNumberBox />
+			  <NineNumberBox />
+			  <NineNumberBox />
+			  <NineNumberBox />
+			  <NineNumberBox />
+			  <NineNumberBox />
+			  <NineNumberBox />
+			  <NineNumberBox />
+			  <NineNumberBox />
 
-			  <div className='square-grid__cell'>
-			    <div className='square-grid__content'>
-			      Some content
-			    </div>
-			  </div>
-
-			  <div className='square-grid__cell'>
-			    <div className='square-grid__content'>
-			      Some content
-			    </div>
-			  </div>
-
-			  <div className='square-grid__cell'>
-				  <div className='square-grid__content'>
-				     Some content
-				  </div>
-			  </div>
-
-			  <div className='square-grid__cell'>
-				  <div className='square-grid__content'>
-				     Some content
-				  </div>
-			  </div>
-
-			  <div className='square-grid__cell'>
-				  <div className='square-grid__content'>
-				     Some content
-				  </div>
-			  </div>
-
-			  <div className='square-grid__cell'>
-				  <div className='square-grid__content'>
-				     Some content
-				  </div>
-			  </div>
-
-			  <div className='square-grid__cell'>
-				  <div className='square-grid__content'>
-				     Some content
-				  </div>
-			  </div>
-
-			  <div className='square-grid__cell'>
-				  <div className='square-grid__content'>
-				     Some content
-				  </div>
-			  </div>
-
+			 
 			</div>  
-		    
-
     	  </div>
     	</div>  
     );
---
+++ app/assets/javascripts/components/nine_number_box.js.jsx
@@ -0,0 +1,13 @@
+var NineNumberBox = React.createClass({
+
+  render: function() {
+    return (
+    	<div className='square-grid__cell'>
+		  <div className='square-grid__content'>
+			  Some content
+		  </div>
+	    </div>
+
+    );
+  }
+});
