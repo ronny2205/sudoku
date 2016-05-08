879ab0df1ded46c2a882aaf237b2b704299a4927
Ronny Almog <ronny2205@gmail.com>
2016-05-08 22:41:48 +0000

    creating tables inside the nine number box

--- app/assets/javascripts/components/nine_number_box.js.jsx
+++ app/assets/javascripts/components/nine_number_box.js.jsx
@@ -4,7 +4,28 @@ var NineNumberBox = React.createClass({
     return (
     	<div className='square-grid__cell'>
 		  <div className='square-grid__content'>
-			  Some content
+            <table className="sudoku-table">
+		        
+		        <tbody>
+		          <tr>
+		            <td className="td-cell col s4">1</td>
+		            <td className="td-cell col s4">2</td>
+		            <td className="td-cell col s4">7</td>
+		          </tr>
+		          <tr>
+		            <td className="td-cell col s4">6</td>
+		            <td className="td-cell col s4">6</td>
+		            <td className="td-cell col s4">6</td>
+		          </tr>
+		          <tr>
+		            <td className="td-cell col s4">4</td>
+		            <td className="td-cell col s4">3</td>
+		            <td className="td-cell col s4">0</td>
+		          </tr>
+		        </tbody>
+		    </table>
+		                
+
 		  </div>
 	    </div>
 
