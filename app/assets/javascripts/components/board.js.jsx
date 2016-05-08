9d767408b03c04df1fbe1c1f7f38be5885d951e8
Ronny Almog <ronny2205@gmail.com>
2016-05-08 00:32:49 +0000

    styling the board

--- app/assets/javascripts/components/board.js.jsx
+++ app/assets/javascripts/components/board.js.jsx
@@ -3,54 +3,64 @@ var Board = React.createClass({
   render: function() {
     return (
     	<div className="row">
-    	  <div className="card-panel teal col s6 offset-s3">
-    		<div className="square">
-			    <div className="content">
-			        square
+    	  <div className="board z-depth-3 col s6 offset-s3">
+    	    <div className='square-grid'>
+			  <div className='square-grid__cell'>
+			    <div className='square-grid__content'>
+			      Some content
 			    </div>
-			</div>
-			<div className="square">
-			    <div className="content">
-			        square
-			    </div>
-			</div>
-			<div className="square">
-			   <div className="content">
-			       square
-			    </div>
-			</div>
+			  </div>
 
-			<div className="square">
-			    <div className="content">
-			        square
+			  <div className='square-grid__cell'>
+			    <div className='square-grid__content'>
+			      Some content
 			    </div>
-			</div>
-			<div className="square">
-			    <div className="content">
-			        square
-			    </div>
-			</div>
-			<div className="square">
-			   <div className="content">
-			       square
-			    </div>
-			</div>
+			  </div>
 
-			<div className="square">
-			    <div className="content">
-			        square
+			  <div className='square-grid__cell'>
+			    <div className='square-grid__content'>
+			      Some content
 			    </div>
-			</div>
-			<div className="square">
-			    <div className="content">
-			        square
-			    </div>
-			</div>
-			<div className="square">
-			   <div className="content">
-			       square
-			    </div>
-			</div>
+			  </div>
+
+			  <div className='square-grid__cell'>
+				  <div className='square-grid__content'>
+				     Some content
+				  </div>
+			  </div>
+
+			  <div className='square-grid__cell'>
+				  <div className='square-grid__content'>
+				     Some content
+				  </div>
+			  </div>
+
+			  <div className='square-grid__cell'>
+				  <div className='square-grid__content'>
+				     Some content
+				  </div>
+			  </div>
+
+			  <div className='square-grid__cell'>
+				  <div className='square-grid__content'>
+				     Some content
+				  </div>
+			  </div>
+
+			  <div className='square-grid__cell'>
+				  <div className='square-grid__content'>
+				     Some content
+				  </div>
+			  </div>
+
+			  <div className='square-grid__cell'>
+				  <div className='square-grid__content'>
+				     Some content
+				  </div>
+			  </div>
+
+			</div>  
+		    
 
     	  </div>
     	</div>  
